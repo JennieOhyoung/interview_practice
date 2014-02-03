23a4ae329d41e5e53343d610c651b50e4a631fca
JennieOhyoung <jennieohyoung@unknown542696dcd235.gateway.pace.com>
2014-02-03 00:46:25 -0800

    7th

--- length_list.py
+++ length_list.py
@@ -1,17 +1,30 @@
-# given a list of consecutive numbers [1...n] where n is unknown, you know whether a number is in list. Find length of list without using built in function len()
+# given a list of consecutive numbers [1...n] where n is unknown, you know whether a number is in list. Find length of list without using built in function len() or slicing. Do this in O(log n)
 
 import random
 
-l = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
-# where we don't know n = 14
+lst = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
 
 def list_gen():
     rand_len = int(random.uniform(1,100))
     rand_list = []
     for num in range(rand_len):
         rand_list.append(num)
-    print rand_list[-1]
+    # print rand_list[-1]
     return rand_list
+
+
+
+# 1. Cheat ==========================================================
+
+def slicing():
+    lst = list_gen()
+    return lst[-1]-lst[0]
+
+print "The length is " + str(slicing())
+
+
+
+# 2. Iterative ===================================================
 
 def len_search(l):
     current = 1
@@ -36,14 +49,38 @@ def len_search(l):
             return current
 
 
-print len_search(list_gen())
+print "Iteratively: " + str(len_search(list_gen()))
 # print len_search(l)
 
 
 
+# 3. Recursive ======================================================
+
+def probe(lst, n):
+    if n <= len(lst):
+        return True
+    return False
+
+def binary_search(low, high):
+    if high - low == 1:
+        return low
+    mid = (high + low) / 2
+    if probe(lst, mid) == False:
+        return binary_search(low, mid)
+    else:
+        return binary_search(mid, high)
+
+def find_len(lst):
+    i = 0
+    while True:
+        if probe(lst, 2**i) == False:
+            break
+        else:
+            i += 1
+    return "Recursively: " + str(binary_search(2**(i-1), 2**i))
 
 
-
+print find_len(list_gen())
 
 
 
