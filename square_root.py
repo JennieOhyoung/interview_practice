7b87402a5e4688ac649669ba5d4af08469070b28
JennieOhyoung <jennieohyoung@unknown542696dcd235.gateway.pace.com>
2014-02-06 11:48:01 -0800

    8th

--- square_root.py
+++ square_root.py
@@ -1,37 +1,24 @@
 # Write a function to find the square root of a number given epsilon (acceptable margin of error)
 
 def sqrt(n, epsilon):
-    upper_x = n
+    upper_x = float(n)
     lower_x = 0
-    mid_pt = float(upper_x + lower_x)/2
-    test = mid_pt*mid_pt
 
     if n < 0 or epsilon < 0:
         return "Both n and epsilon have be greater than 0"
 
     while True:
-        mid_pt = float(upper_x + lower_x)/2
+        mid_pt = (upper_x + lower_x)/2
         test = mid_pt*mid_pt
 
         if test > n:
             upper_x = mid_pt
-            mid_pt = float(upper_x + lower_x)/2
 
         if test < n:
             lower_x = mid_pt
-            mid_pt = float(upper_x + lower_x)/2
 
-        if n-epsilon < test < n+epsilon:
+        if n-epsilon < mid_pt*mid_pt< n+epsilon:
             return mid_pt
-        
 
-
-print sqrt(139, 0.1)
-print sqrt(100.92, 0.000001)
-print sqrt(1523, -0.01)
-
-
-
-def sqrt_recursion(n, epsilon):
-    
+print sqrt(100, 15)
 
