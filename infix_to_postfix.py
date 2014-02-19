255a1275815360dde33d1331d1063475e7d406fe
JennieOhyoung <jennieohyoung@unknown542696dcd235.gateway.pace.com>
2014-02-19 11:40:13 -0800

    10th

---
+++ infix_to_postfix.py
@@ -0,0 +1,112 @@
+
+"""
+write functions to convert Infix expressions to Postfix, another to evaluate.
+"""
+
+class Stack(object):
+    def __init__(self):
+        self.items = []
+
+    def __str__(self):
+        return str(self.items)
+
+    def isEmpty(self):
+        return self.items == []
+
+    def push(self, item):
+        return self.items.append(item)
+
+    def pop(self):
+        return self.items.pop()
+
+    def peek(self):
+        return self.items[-1]
+
+    def size(self):
+        return len(self.items)
+
+
+################################################################################
+
+
+def order_operation(operator):
+    if operator in "^":
+        return 3
+    elif operator in "*/":
+        return 2
+    elif operator in "+-":
+        return 1
+    else:
+        return 0
+
+def postfix_conversion(infix):
+    s = Stack()
+    output = []
+    infix_list = infix.split()
+
+    for i in infix_list:
+        if i in "0123456789" or i in "ABCDEFGHIJKLMNOPQRSTUVWXYZ":
+            output.append(i)
+        elif i == "(":
+            s.push(i)
+        elif i == ")":
+            top = s.pop()
+            while top != "(":
+                output.append(top)
+                top = s.pop()
+        else:
+            while (not s.isEmpty()) and (order_operation(s.peek()) >= order_operation(i)):
+                output.append(s.pop())
+            s.push(i)
+
+    while not s.isEmpty():
+        output.append(s.pop())
+
+    return "".join(output)
+
+# print postfix_conversion("A * B + C * D")
+# print postfix_conversion("A + B * C + D")
+# print postfix_conversion("( A + B ) * C + D")
+# print postfix_conversion("A + B + C + D")
+# print postfix_conversion("( A + B ) * C - ( D - E ) * ( F + G )")
+# print postfix_conversion("5 * 3 ^ ( 4 - 2 )")
+
+
+
+################################################################################
+
+def math(op, op1, op2):
+    if op =="*":
+        return op1*op2
+    elif op == "/":
+        return op1/op2
+    elif op == "+":
+        return op1+op2
+    else:
+        return op1-op2
+
+def postfix_eval(infix):
+    s = Stack()
+    infix_list = infix.split()
+
+    for i in infix_list:
+        if i in "0123456789":
+            s.push(int(i))
+        else:
+            op2 = s.pop()
+            op1 = s.pop()
+            result = math(i, op1, op2)
+            s.push(result)
+    return s.pop()
+
+
+print postfix_eval("7 8 + 3 2 + /")
+print postfix_eval("4 8 / 3 6 * ")
+print postfix_eval("17 10 + 3 * 9 / ==")
+print postfix_eval("5 * 3 ^ (4 - 2)")
+
+
+
+
+
+
---
+++ phonescreen_practice.py
@@ -0,0 +1,97 @@
+# Write a function to reverse a string.
+n = "hello world"
+def reverse(n):
+    # return n[::-1]
+    temp = n[0]
+    n_list = []
+    for i in range(len(n)):
+        n_list.append(n[-1-i])
+    return ''.join(n_list)
+
+
+# Write function to compute Nth fibonacci number
+def fib(n):
+    if n <= 2:
+        return 1
+    return fib(n-1)+fib(n-2)
+
+# print fib(6)
+def fib2(n):
+    fib = [0,1]
+    for i in range(1,n):
+        fib.append(fib[-1]+fib[-2])
+    return fib[-1]
+
+#Print out the grade-school multiplication table
+def multiplication_table(w,h):
+    for i in range(1,h+1):
+        for j in range(1,w+1):
+            print i*j, "\t",
+        print
+
+
+# Write function to print the odd numbers from 1 to 99.
+def odd_num(num):
+    for i in range(1,num+1):
+        if i%2 != 0:
+            print i
+
+
+# Find the largest int value in an int array.
+def largest_int(array):
+    max = array[1]
+    for i in array:
+        if i>max:
+            max = i
+    return max
+
+
+# Format an RGB value (three 1-byte numbers) as a 6-digit hexadecimal string.
+class Stack(object):
+    def __init__(self):
+        self.items = []
+
+    def __str__(self):
+        return str(self.items)
+
+    def isEmpty(self):
+        return self.items == []
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        return self.items.pop()
+
+    def peek(self):
+        return self.items[-1]
+
+    def size(self):
+        return len(self.items)
+
+def rbg_converter(rbg):
+    hex_val = []
+    hex_digits = "0123456789ABCDEF"
+    s = Stack()
+
+    for color in rbg:
+        while color >0:
+            rem = color%16
+            s.push(rem)
+            color = color//16
+
+        hexstring = ''
+        while not s.isEmpty():
+            hexstring=hexstring+hex_digits[s.pop()]
+        hex_val.append(hexstring)
+    return ''.join(hex_val)
+
+
+rbg = [233,24, 255]
+print rbg_converter(rbg)
+
+
+
+
+
+
